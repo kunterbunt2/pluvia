d78859ebc0d9bb7caeaae5f4ff161b5890bd0f55
kunterbunt <abdalla@bushnaq.de>
2022-06-25 12:12:29 +0200

    linux script also works without a tag version

--- build_app_linux.sh
+++ build_app_linux.sh
@@ -15,6 +15,11 @@ MAIN_JAR="pluvia-$PROJECT_VERSION.jar"
 # Set desired installer type: "app-image", "rpm" or "deb".
 INSTALLER_TYPE=deb
 
+if [ -z "$TAG_NAME" ]
+then
+  TAG_NAME=$PROJECT_VERSION
+fi
+
 echo "java home: $JAVA_HOME"
 echo "project version: $PROJECT_VERSION"
 echo "app version: $APP_VERSION"
