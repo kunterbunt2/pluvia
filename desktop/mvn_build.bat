64282c8505ed2d036b2f6c6da117e158e204e7b9
kunterbunt <abdalla@bushnaq.de>
2022-07-30 14:51:13 +0200

    fixed github wirkflows and batch files

--- desktop/mvn_build.bat
+++ desktop/mvn_build.bat
@@ -1,7 +1,9 @@
 call mvn -version
 cd ..
 call mvn clean install
-cd core
+cd engine
+call mvn clean install
+cd ../core
 call mvn clean install
 cd ../desktop
 call mvn clean install
--- desktop/mvn_build.sh
+++ desktop/mvn_build.sh
@@ -3,7 +3,9 @@
 mvn -version
 cd ..
 mvn clean install
-cd core
+cd engine
+mvn clean install
+cd ../core
 mvn clean install
 cd ../desktop
 mvn clean install
